5d882b7a2f60803952c8869b259d87abcfd3fc03
Reema Patel <reemapatel17@outlook.com>
2019-09-21 07:38:23 +0100

    cleaned-up code

--- scripts/app.js
+++ scripts/app.js
@@ -1,62 +1,68 @@
+let playerIndex = 95
+let direction = 1
+const width = 10
+const cells = []
+let alienIndex = 1
+
+function spaceBar () {
+  const spaceBar = setInterval(() => {
+    console.log('shoot')
+  }, 500)
+  setTimeout(() => {
+    clearInterval(spaceBar)    
+  }, 5000)
+}
+function handleUserInput(keyCode) {
+  switch (keyCode) {
+    case 37: if (playerIndex % width > 0) playerIndex -= 1
+      break
+    case 39: if (Math.floor(playerIndex % width < width - 1)) playerIndex += 1
+      break
+    case 32: spaceBar()
+      break    
+  }
+}
+function alienMovement () {
+  alienIndex += direction
+  const x = Math.floor(alienIndex % width)
+  if (direction === 1 && x === width - 1) {
+    direction = width
+  } else if (direction === width && x === width - 1) {
+    direction = -1
+  } else if (direction === -1 && x === 0) {
+    direction = width
+  } else if (direction === width && x === 0) {
+    direction = 1
+  }
+}
+
+
+// Load DOM
 window.addEventListener('DOMContentLoaded', () => {
 
   const grid = document.querySelector('.grid')
-  const width = 10
-  const cells = []
-  let playerIndex = 95
-  let direction = 1
-  // let bullet = playerIndex - width
   
-
-  // create cells within the grid
+  // grid with cells - added 'cell' to the array 'cells'
   for (let i = 0; i < width ** 2; i++) {
     const cell = document.createElement('div')
     grid.appendChild(cell)
     cells.push(cell)
   }
 
-  // movement of player - arrow keys(left & right)
-  // player stops at wall
+  // EventListener for movement of Player - arrow keys
   cells[playerIndex].classList.add('player')
-  document.addEventListener('keyup', (e) => {
+  document.addEventListener('keydown', (e) => {
     // console.log('works')
     cells[playerIndex].classList.remove('player')
-
-    switch (e.keyCode) {
-      case 37: if (playerIndex % width > 0) { 
-        playerIndex -= 1
-      }
-        break
-      case 39: if (Math.floor(playerIndex % width < width - 1)) playerIndex += 1
-        break
-    }
+    handleUserInput(e.keyCode)
     cells[playerIndex].classList.add('player')
   })
 
-  // setInterval(() => {
-  //   bullet += 1
-  //   cells[bullet].classList.add('bullet')
-  // }, 500)
-
-
-  let alienIndex = 1
+  // Set Interval for Alien
   cells[alienIndex].classList.add('alien')
   setInterval(() => {
-    // console.log('moves')
     cells[alienIndex].classList.remove('alien')    
-
-    alienIndex += direction
-    const x = Math.floor(alienIndex % width)
-
-    if (direction === 1 && x === width - 1) {
-      direction = width
-    } else if (direction === width && x === width - 1) {
-      direction = -1
-    } else if (direction === -1 && x === 0) {
-      direction = width
-    }
-    
-
+    alienMovement()
     cells[alienIndex].classList.add('alien')
   }, 300)
 
