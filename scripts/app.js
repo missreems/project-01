1a388aa3eb551379085957436a1ab5a6b382f841
Reema Patel <reemapatel17@outlook.com>
2019-09-24 18:48:10 +0100

    updated css & wave of aliens

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,14 @@
 // VARIABLE NAMES------------------------------------------------------------
 const width = 10
 const cells = []
-let direction = 1
 
 let playerIdx = 95
-const aliens = [1, 3, 5, 7]
 let playerBullet = playerIdx - width
-// const alienBullets = aliens.map(alien => {
-//   alien + width
-//   // console.log(alien + width)
-// }) 
+
+let aliens1 = [4, 13, 15, 22, 26]
+let aliens2 = [0, 11, 2]
+let firstWave = false
+
 
 
 function handleUserInput(keyCode) {
@@ -21,27 +20,6 @@ function handleUserInput(keyCode) {
     case 32: spaceBar()
       break
   }
-}
-
-
-function alienMovement() {
-  aliens.forEach(alien => {
-    
-    alien += direction
-  
-    const x = Math.floor(alien % width)
-
-    if (direction === 1) {
-      direction = 1
-    } else if (direction === width && x === width - 1) {
-      direction = -1
-    } else if (direction === -1 && x === 0) {
-      direction = width
-    } else if (direction === width && x === 0) {
-      direction = 1
-    } 
-  })
-  
 }
 function spaceBar() {
   cells[playerBullet].classList.add('bullet')
@@ -64,20 +42,26 @@ function spaceBar() {
 
 // COLLISION - YOU LOSE!------------------------------------------------------------
 // player shoots alien
-// alien shoots player
-// player's bullet and alien's bullet clashes - should cancel out
 function collision() {
-  // aliens.forEach(alien => {
-  //   if (alien[alienBullet] === playerIdx) {
-  //     console.log('end of game')
-  //   }
-  // })
-  aliens.forEach(alien => {
+  // ALIENS 1
+  // if (cells.classList.includes(alien) && cells.classList.includes(bullet)) {
+  //   console.log('shot alien1')
+  // }
+  aliens1.forEach(alien => {
     if (playerBullet === alien) {
-      console.log('alien shot')
+      console.log('alien 1 shot')
+    }
+  })
+
+  // ALIENS 2
+  aliens2.forEach(alien => {
+    if (playerBullet === alien) {
+      console.log('alien 2 shot')
     }
   })
 }
+
+
   
 
 
@@ -85,6 +69,7 @@ function collision() {
 window.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid')
 
+   
 
   // GRID WITH CELLS------------------------------------------------------------
   for (let i = 0; i < width ** 2; i++) {
@@ -103,40 +88,54 @@ window.addEventListener('DOMContentLoaded', () => {
   })
 
   
-  // ALIEN MOVEMENT PATTERN------------------------------------------------------------
+  // ALIENS MOVEMENT PATTERN------------------------------------------------------------
+  // ALIENS 1
 
-  aliens.forEach(alien => {
+  function checkFour (alien) {
+    if (!firstWave && alien === 70) {
+      console.log('should start second wave now')
+      moveSecondWave()
+    }
+  }
+  
+  aliens1.forEach((alien, i) => {
     cells[alien].classList.add('alien')
-    const alienSetInterval = setInterval(() => {
+
+    const aliens1Moving = setInterval(() => {
       cells[alien].classList.remove('alien')
-      alienMovement()
-      // ALIEN REACHES BOTTOM OF SCREEN - YOU LOSE!
-      //change 79 - make flexible for all grid sizes
+      alien += 1
+      
+      if (i === 4) {
+        checkFour(alien)
+      }
+
       if (alien > 79) {
-        clearInterval(alienSetInterval)
+        clearInterval(aliens1Moving)
         console.log('end of game')
       }
       cells[alien].classList.add('alien')
-    }, 500)
+    }, 300)
   })
 
+  
 
-
-  // ALIEN SHOOTS A BULLET------------------------------------------------------------
-  aliens.forEach(alienBullet => {
-    // cells[Math.floor(Math.random() * cells.length)] - finds random cell within cells
-    cells[alienBullet].classList.add('alienBullet')
-    const alienBulletMoving = setInterval(() => {
-      cells[alienBullet].classList.remove('alienBullet')
-      if (alienBullet < 89) {
-        console.log(alienBullet)
-        alienBullet += width
-        collision()
-        cells[alienBullet].classList.add('alienBullet')
-      } else {
-        cells[alienBullet].classList.remove('alienBullet')
-        clearInterval(alienBulletMoving)
-      }
-    }, 500)
-  }) 
+  // ALIENS 2
+  function moveSecondWave() {
+    aliens2.forEach(alien => {
+      cells[alien].classList.add('alien')
+  
+      const aliens2Moving = setInterval(() => {
+        cells[alien].classList.remove('alien')
+        alien += 1
+  
+        // ALIEN REACHES BOTTOM OF SCREEN - YOU LOSE!
+        if (alien > 79) {
+          clearInterval(aliens2Moving)
+          console.log('end of game')
+        }
+        cells[alien].classList.add('alien')
+      }, 300)
+    })
+  }
+  
 })
