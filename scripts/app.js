5d5de2ac761ad0c1c0802f99238a7ed4c598440b
Reema Patel <reemapatel17@outlook.com>
2019-09-26 18:39:32 +0100

    added winning & losing graphics

--- scripts/app.js
+++ scripts/app.js
@@ -5,107 +5,73 @@ const cells = []
 let playerIdx = 95
 let playerBullet = playerIdx - width
 
-let aliens1 = [4, 13, 15, 22, 26]
-let aliens2 = [1, 12, 3]
-let firstWave = false
+let aliens1 = [79]
+// let aliens1 = [4, 13, 15, 22, 26]
+// let aliens2 = [1, 12, 3]
+// let firstWave = false
 let aliens2Moving
 let aliens1Moving
 // let result = document.querySelector('.result')
 
 
-function handleUserInput(keyCode) {
-  switch (keyCode) {
-    case 37: if (playerIdx % width > 0) playerIdx -= 1
-      break
-    case 39: if (Math.floor(playerIdx % width < width - 1)) playerIdx += 1
-      break
-    case 86: spaceBar()
-      break
-  }
-}
-function spaceBar() {
-  cells[playerBullet].classList.add('bullet')
-
-  const bulletMoving = setInterval(() => {
-    cells[playerBullet].classList.remove('bullet')
-    
-    if (playerBullet > 10 && !cells[playerBullet].classList.contains('alien')) {
-      playerBullet -= width
-      cells[playerBullet].classList.add('bullet')
-    } else {
-      aliens1 = aliens1.filter(alien => {
-        return alien !== playerBullet + 1
-      })
-      cells[playerBullet].classList.remove('bullet', 'alien')
-      cells[playerBullet].classList.add('explosion')
-      setTimeout(() => {
-        cells[playerBullet].classList.remove('explosion')
-      }, 300)
-      clearInterval(bulletMoving)
-    }
-  }, 100)
-
-  cells[playerBullet].classList.remove('bullet')
-  playerBullet = playerIdx - width
-}
 
 
-// function spaceBar() {
-  
-//   const bulletMoving = setInterval(() => {
-//     cells[playerBullet].classList.remove('bullet')
-//     if (cells[playerBullet].classList.contains('alien')) {
-//       aliens1 = aliens1.map(alien => {
-//         if (!alien) return null
-        
-//         console.log(playerBullet, 'bullet')
-//         console.log(alien, 'alien')
 
-//         if (playerBullet + 1 === alien) {
-          
-          
-//           console.log(cells[playerBullet - width].classList)
-//           cells[playerBullet - width].classList[0] = ''
-//           console.log(cells[playerBullet - width].classList)
-          
-//           clearInterval(bulletMoving)
-//           //  Code from Olly
-//           //  if (cells[laserIdx].classList.contains(‘alien’)) {
-//           //   cells[laserIdx].classList.remove(‘alien’, ‘laser’)
-        
-//           // cells[playerBullet].classList.remove('bullet')
-//           // clearInterval(bulletMoving)
-//           return null
-//         }
-//         return alien
-//       })
-//     }
-//     if (playerBullet > 10) {
-//       playerBullet -= width
-//       cells[playerBullet].classList.add('bullet')
-//     } else {
-//       cells[playerBullet].classList.remove('bullet')
-//       clearInterval(bulletMoving)
-//     }
-//   }, 300)
 
-//   cells[playerBullet].classList.remove('bullet')
-//   playerBullet = playerIdx - width
-// }
- 
-// WINNING CONDITION ------------------------------------------------------------
-// aliens1.every(alien => {
-//   if (alien === null) alert('You win!')
-// })
 
-if (!aliens1.length) alert('you won')
 
 
 // LOAD DOM------------------------------------------------------------
 window.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid')
+  const winningText = document.querySelector('.winning-text')
+  winningText.classList.add('hide')
+  const losingText = document.querySelector('.losing-text')
+  losingText.classList.add('hide')
+  const reset = document.querySelector('.reset')
+  reset.classList.add('hide')
 
-   
+
+  function handleUserInput(keyCode) {
+    switch (keyCode) {
+      case 37: if (playerIdx % width > 0) playerIdx -= 1
+        break
+      case 39: if (Math.floor(playerIdx % width < width - 1)) playerIdx += 1
+        break
+      case 86: spaceBar()
+        break
+    }
+  }
+
+  
+  // WINNING CONDITION ------------------------------------------------------------
+  function win() {
+    if (!aliens1.length) {
+      console.log('you win!')
+      grid.classList.replace('grid', 'hide')
+      winningText.classList.remove('hide')
+      winningText.classList.add('.winning-text')
+      reset.classList.remove('hide')
+      reset.classList.add('.reset')
+    }
+  }
+
+  // LOSING CONDITION ------------------------------------------------------------
+  function lose() {
+    console.log('you win!')
+    grid.classList.replace('grid', 'hide')
+    losingText.classList.remove('hide')
+    losingText.classList.add('.losing-text')
+    reset.classList.remove('hide')
+    reset.classList.add('.reset')
+    
+  }
+
+  // RESET BUTTON ------------------------------------------------------------
+  reset.addEventListener('click', () => {
+    location.reload()
+  })
+
 
   // GRID WITH CELLS------------------------------------------------------------
   for (let i = 0; i < width ** 2; i++) {
@@ -122,26 +88,51 @@ window.addEventListener('DOMContentLoaded', () => {
     cells[playerIdx].classList.add('player')
   })
 
+  function spaceBar() {
+    cells[playerBullet].classList.add('bullet')
+  
+    const bulletMoving = setInterval(() => {
+      cells[playerBullet].classList.remove('bullet')
+  
+      if (playerBullet > 10 && !cells[playerBullet].classList.contains('alien')) {
+        playerBullet -= width
+        cells[playerBullet].classList.add('bullet')
+      } else {
+        aliens1 = aliens1.filter(alien => {
+          return alien !== playerBullet + 1
+        })
+        cells[playerBullet].classList.remove('bullet', 'alien')
+        cells[playerBullet].classList.add('explosion')
+        setTimeout(() => {
+          cells[playerBullet].classList.remove('explosion')
+        }, 300)
+        clearInterval(bulletMoving)
+        win()
+      }
+    }, 100)
+  
+    cells[playerBullet].classList.remove('bullet')
+    playerBullet = playerIdx - width
+  }
 
   // ALIENS 1 MOVEMENT PATTERN------------------------------------------------------------
-  function checkFour (alien) {
-    if (!firstWave && alien === 70) {
-      moveSecondWave()
-      firstWave = true
-    }
-  }
+  // function checkFour (alien) {
+  //   if (!firstWave && alien === 50) {
+  //     moveSecondWave()
+  //     firstWave = true
+  //   }
+  // }
 
   aliens1Moving = setInterval(() => {
     cells.forEach(cell => cell.classList.remove('alien'))
-    aliens1.forEach((alien, i) => {
-      if (i === 4) {
-        checkFour(alien)
-      }
+    aliens1.forEach((alien) => {
+      // if (i === 4) {
+      //   checkFour(alien)
+      // }
       if (alien >= 81) {
         clearInterval(aliens1Moving)
         clearInterval(aliens2Moving)
-        console.log('you lose!')
-        // result.textContent = 'YOU LOSE!'
+        lose()
         cells.forEach(cell => cell.classList.remove('alien'))
         cells[playerIdx].classList.remove('player')
       }
@@ -153,30 +144,30 @@ window.addEventListener('DOMContentLoaded', () => {
       if (!alien) return null
       return alien + 1
     })
-  }, 500)
+  }, 300)
 
 
   // ALIENS 2 MOVEMENT PATTERN------------------------------------------------------------
-  function moveSecondWave() {
-    cells.forEach(cell => cell.classList.remove('alien'))
-    aliens2Moving = setInterval(() => {
-      aliens2.forEach((alien) => {
-        if (alien >= 81) {
-          clearInterval(aliens1Moving)
-          clearInterval(aliens2Moving)
-          console.log('you lose!')
-          // result.textContent = 'YOU LOSE!'
-          cells.forEach(cell => cell.classList.remove('alien'))
-          cells[playerIdx].classList.remove('player') 
-        }
-        if (alien) {
-          cells[alien].classList.add('alien')
-        }
-      })
-      aliens2 = aliens2.map(alien => {
-        if (!alien) return null
-        return alien + 1
-      })
-    }, 500)
-  }
+  // function moveSecondWave() {
+  //   cells.forEach(cell => cell.classList.remove('alien'))
+  //   aliens2Moving = setInterval(() => {
+  //     aliens2.forEach((alien) => {
+  //       if (alien >= 81) {
+  //         clearInterval(aliens1Moving)
+  //         clearInterval(aliens2Moving)
+  //         console.log('you lose!')
+  //         // result.textContent = 'YOU LOSE!'
+  //         cells.forEach(cell => cell.classList.remove('alien'))
+  //         cells[playerIdx].classList.remove('player') 
+  //       }
+  //       if (alien) {
+  //         cells[alien].classList.add('alien')
+  //       }
+  //     })
+  //     aliens2 = aliens2.map(alien => {
+  //       if (!alien) return null
+  //       return alien + 1
+  //     })
+  //   }, 500)
+  // }
 })
